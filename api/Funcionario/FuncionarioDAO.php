abd82dc4c47206dd08b4710b4d0f38084b7bd91b
Breno Limagit config --global user.name Breno <brenolimanf@gmail.com>
2023-03-08 14:05:34 -0300

    cadastrarAluno

--- api/Funcionario/FuncionarioDAO.php
+++ api/Funcionario/FuncionarioDAO.php
@@ -1,7 +1,37 @@
 <?php
 
-class FuncionarioDAO{
+require_once('RepositorioFuncionarioDAO.php');
+require_once('../Exception/FuncionarioException.php');
+/**
+ * Summary of FuncionarioDAO
+ */
+class FuncionarioDAO implements RepositorioFuncionarioDAO{
 
+    private $pdo;
+
+    /**
+     * Summary of __construct
+     * @param mixed $pdo
+     */
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+	/**
+	 * @param Aluno $aluno
+	 * @return mixed
+	 */
+	public function cadastrarAlunos(Aluno $aluno) {
+        try {
+            $this->pdo->beginTransaction();
+            $ps = $this->pdo->prepare('INSERT INTO alunos(matricula, nome, cpf, telefone, email) (?,?,?,?,?)');
+            $ps->execute([$aluno->getMatricula(), $aluno->getNome(), $aluno->getCpf(), $aluno->getTelefone(), $aluno->getEmail()]);
+            return true;
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            throw new FuncionarioException('Falha ao cadastrar aluno: ' . $e->getMessage());
+        }
+	}
 }
 
 ?>
---
+++ api/Funcionario/RepositorioFuncionarioDAO.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Summary of RepositorioFuncionarioDAO
+ */
+interface RepositorioFuncionarioDAO{
+
+    /**
+     * Summary of cadastroAlunos
+     * @param Aluno $aluno
+     * @return void
+     */
+    public function cadastrarAlunos(Aluno $aluno);
+
+}
+
+?>
