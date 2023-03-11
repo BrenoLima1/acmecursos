8ee8af5c2b29cccd0ce3517edf9b7edf081d463a
Breno Limagit config --global user.name Breno <brenolimanf@gmail.com>
2023-03-11 00:15:55 -0300

    Validação de funcionário

--- api/Funcionario/RepositorioFuncionarioDAO.php
+++ api/Funcionario/RepositorioFuncionarioDAO.php
@@ -19,12 +19,33 @@ interface RepositorioFuncionarioDAO{
      */
     public function validarFuncionarioCPF(Funcionario $funcionario): bool;
 
+    /**
+     * Summary of validarFuncionarioEmail
+     * @param Funcionario $funcionario
+     * @return bool
+     */
     public function validarFuncionarioEmail(Funcionario $funcionario): bool;
 
+    /**
+     * Summary of cadastrarFuncionario
+     * @param Funcionario $funcionario
+     * @return bool
+     */
     public function cadastrarFuncionario(Funcionario $funcionario): bool;
 
+    /**
+     * Summary of removerFuncionario
+     * @param mixed $id
+     * @return bool
+     */
     public function removerFuncionario($id): bool;
 
+    /**
+     * Summary of alterarFuncionario
+     * @param Funcionario $funcionario
+     * @param mixed $id
+     * @return bool
+     */
     public function alterarFuncionario(Funcionario $funcionario, $id): bool;
 }
 
