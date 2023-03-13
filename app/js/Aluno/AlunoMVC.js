36fd927ea3ea9de07bad43db0340f17702d5e2f9
Breno Lima <brenolimanf@gmail.com>
2023-03-13 19:29:57 -0300

    AlunoMVC

--- app/js/Aluno/AlunoMVC.js
+++ app/js/Aluno/AlunoMVC.js
@@ -18,12 +18,36 @@ async function buscarAlunos(){
     })
 }
 
-async function cadastrarAluno() {
+async function criarAluno(){
     const matricula = document.getElementById('matricula').value;
     const nome = document.getElementById('nome').value;
     const cpf = document.getElementById('cpf').value;
     const telefone = document.getElementById('telefone').value;
     const email = document.getElementById('email').value;
-
     const aluno = new Aluno(matricula, nome, cpf, telefone, email);
+    return aluno;
+}
+
+async function cadastrarAluno() {
+    const formData = new FormData(document.querySelector('form'));
+
+    const resposta = await fetch(API + 'aluno', {
+        method:'POST',
+        body:formData
+    })
+    .then(async (resposta) => {
+        const output = document.querySelector('output');
+        if(!resposta.ok){
+            output.innerText = 'Falha ao cadastrar aluno.';
+            alert('Falha ao cadastrar aluno.' + '\n' + resposta.status);
+        }
+
+        if(resposta.status == 409){
+            output.innerText = 'Erro: CPF já cadastrado anteriormente.';
+            alert('Erro: CPF já cadastrado anteriormente.');
+        }else if(resposta.status == 422){
+            output.innerText = 'Erro: matrícula já existente';
+            alert('Erro: matrícula já existente.');
+        }
+    })
 }
